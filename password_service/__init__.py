8201d6ea70725736df5b2e5fa75b8ce72d8a811f
Marcos Mdo <Marcosmdo@knights.ucf.edu>
2019-05-07 18:11:08 -0400

    Finished API calls. Cleaned code and added comments. updated parsing functions to take care of end line symbols.

--- password_service/__init__.py
+++ password_service/__init__.py
@@ -6,27 +6,28 @@ from .helper import parseToJson
 import sys
 app = Flask(__name__)
 
-# Set sort off to return JSON in expected order.
+# Set sort keys off to return JSON in expected order.
 app.config['JSON_SORT_KEYS'] = False
 
 # base route path. Displays README file.
-@app.route("/")
+@app.route("/", methods=['GET'])
 def index():
     with open(os.path.dirname(app.root_path) + '/README.md', 'r') as file:
-
         content = file.read()
+
         return markdown.markdown(content)
 
 # Get all users method. Returns list of JSON objects
 @app.route("/users", methods=['GET'])
 def getAllUsers():
     result = []
+
     with open(os.path.dirname(app.root_path) + '/passwd', 'r') as users:
         content = users.readline()
 
         while content:
             registration = content.split(':')
-            json_data = parseToJson.parseUser(registration)
+            json_data = parseToJson.parseUser(registration) # helper function to parse JSON
             result.append(json_data)
 
             content = users.readline()
@@ -36,10 +37,9 @@ def getAllUsers():
 # Get users via query, where only exact matches are allowed
 # currently works, but way too slow when querying. will need to store
 # passwd and group data in local db and quick access.
-@app.route("/users/query")
+@app.route("/users/query", methods=['GET'])
 def getUsersByQuery():
     args = request.args
-    parse.urlencode(args)
     result = []
     flag = True
 
@@ -55,7 +55,6 @@ def getUsersByQuery():
                 if str(json_data[key]) != str(value):
                     flag = False
             if flag:
-                print("appending data.", file=sys.stderr)
                 result.append(json_data)
 
             content = users.readline()
@@ -64,16 +63,17 @@ def getUsersByQuery():
 
 
 # Get a user with a specific UID.
-@app.route("/users/<string:uid>")
+@app.route("/users/<string:uid>", methods=['GET'])
 def getUserByUid(uid):
     with open(os.path.dirname(app.root_path) + '/passwd', 'r') as users:
         content = users.readline()
 
         while content:
             registration = content.split(':')
-
             user_uid = registration[2]
 
+            # Check if the passed in uid matches with the current contents uid.
+            # If it does, parse JSON and return the found content.
             if int(user_uid) == int(uid):
                 json_data = parseToJson.parseUser(registration)
                 return jsonify(json_data)
@@ -82,34 +82,39 @@ def getUserByUid(uid):
     abort(404)
 
 # Get the groups of a user with a specific UID.
-@app.route("/users/<string:uid>/groups")
+@app.route("/users/<string:uid>/groups", methods=['GET'])
 def getUserGroupsByUid(uid):
     username = None
     result = []
+
     with open(os.path.dirname(app.root_path) + '/passwd', 'r') as users:
         content = users.readline()
 
         while content:
             registration = content.split(':')
-
             user_uid = registration[2]
 
+            # Check if the passed in uid matches with the current contents uid.
+            # If it does, store username information, and scan through the groups 
+            # file for a matching group with the username.
             if int(user_uid) == int(uid):
-                # scan this user in the groups file for their username
                 username = registration[0]
                 break
 
             content = users.readline()
 
     if username is None:
-        return jsonify({})
+        return jsonify([])
 
     with open(os.path.dirname(app.root_path) + '/group', 'r') as groups:
         content = groups.readline()
 
         while content:
             registration = content.split(':')
-
+            
+            # We need to temporarily store the members of the content to compare.
+            # The last member in the list is appended with a newline symbol, so 
+            # we must strip it off of the members name for proper comparison.
             members = registration[3].split(',')
             members[-1] = members[-1].rstrip()
 
@@ -125,12 +130,12 @@ def getUserGroupsByUid(uid):
 @app.route("/groups", methods=['GET'])
 def getAllGroups():
     result = []
+
     with open(os.path.dirname(app.root_path) + '/group', 'r') as groups:
         content = groups.readline()
 
         while content:
             registration = content.split(':')
-
             json_data = parseToJson.parseGroup(registration)
             result.append(json_data)
 
@@ -139,12 +144,12 @@ def getAllGroups():
     return jsonify(result)
 
 # Return a list of groups matching all of the specified query fields.
-@app.route("/groups/query")
+@app.route("/groups/query", methods=['GET'])
 def getGroupsByQuery():
-    args = request.args
-    parse.urlencode(args, doseq=True)
-    for i in args.items():
-        print(i, file=sys.stderr)
+    # We used args = request.args in the /users/query API call. 
+    # Because we allow multiples of the same key (in this case, members) we need to parse the query 
+    # parameters differently. Here we use parse_qs to parse the query string provided from the request
+    argdict = parse.parse_qs(request.query_string.decode('utf8'))
     result = []
     flag = True
 
@@ -152,44 +157,53 @@ def getGroupsByQuery():
         content = groups.readline()
 
         while content:
+            # The flag boolean will be used to determine if the content is passable by all parameters.
+            # Because there are multiple checks, we need to hold the flags boolean value for final appending.
             flag = True
+
+            # holds all members passed in via parameter. The ONLY variable that holds multiple values.
+            members = [] 
+
             registration = content.split(':')
             json_data = parseToJson.parseGroup(registration)
-            members = []
-            for key, value in args.items():
-                print("Value is: " + value, file=sys.stderr)
-                if key == 'member':
-                    members.append(value)
-                elif str(json_data[key]) != str(value):
+        
+            for key, value in argdict.items():
+                # urllib.parse_qs returns the parsed query string as a Multidict object.
+                # Due to this implementation, we need to access the first indice.
+                # We only worry about the first indice of the keys that are not members because out API only
+                # accepts duplicates of member values.  
+                if key  == 'member':
+                    for j in value:
+                        members.append(j)
+                elif json_data[key][0] != value[0]:
                     flag = False
+                
+            # Iterate all appended members from the query string and compare to all members found in this 
+            # content, or group found in the /etc/group file.
             for m in members:
-                print(m, file=sys.stderr)
                 if m not in json_data['members']:
                     flag = False
+
             if flag:
-                print("appending data.", file=sys.stderr)
                 result.append(json_data)
 
             content = groups.readline()
 
     return jsonify(result)
 
-
-
 # Return a single group with <gid>. Return 404 if <gid> is not found.
-@app.route("/groups/<string:gid>")
+@app.route("/groups/<string:gid>", methods=['GET'])
 def getGroupbyGid(gid):
     with open(os.path.dirname(app.root_path) + '/group', 'r') as groups:
         content = groups.readline()
 
         while content:
             registration = content.split(':')
-
             user_gid = registration[2]
 
             if int(user_gid) == int(gid):
-
                 json_data = parseToJson.parseGroup(registration)
+                
                 return jsonify(json_data)
 
             content = groups.readline()
--- password_service/helper/parseToJson.py
+++ password_service/helper/parseToJson.py
@@ -1,12 +1,12 @@
 def parseUser(registration):
     json_data = {}
-
-    json_data['name'] = registration[0]
-    json_data['uid'] = registration[2]
-    json_data['gid'] = registration[3]
-    json_data['comment'] = registration[4]
-    json_data['home'] = registration[5]
-    json_data['shell'] = registration[6]
+    
+    json_data['name'] = registration[0].rstrip()
+    json_data['uid'] = registration[2].rstrip()
+    json_data['gid'] = registration[3].rstrip()
+    json_data['comment'] = registration[4].rstrip()
+    json_data['home'] = registration[5].rstrip()
+    json_data['shell'] = registration[6].rstrip()
 
     return json_data
 
@@ -16,8 +16,8 @@ def parseGroup(registration):
     members = registration[3].split(',')
     members[-1] = members[-1].rstrip()
 
-    json_data['name'] = registration[0]
-    json_data['gid'] = registration[2]
+    json_data['name'] = registration[0].rstrip()
+    json_data['gid'] = registration[2].rstrip()
     json_data['members'] = members
 
     return json_data

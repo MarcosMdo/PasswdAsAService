d7864def0e6e3b8508a56183a432cb08aab489d2
Marcos Mdo <Marcosmdo@knights.ucf.edu>
2019-05-10 01:12:34 -0400

    add gitignore

--- initialize.sh
+++ initialize.sh
@@ -2,14 +2,17 @@
 
 if [[ $# -eq 0 ]] # Case for no arguments passed.
 then
-	cp /etc/passwd etc/
+	rm -rf etc
+    mkdir etc
+    cp /etc/passwd etc/
     cp /etc/group etc/
     docker-compose build
     docker-compose up
 
 elif [[ $# -ne 0 ]] # Case for more than one argument passed
 then
-    rm -rf etc/*
+    rm -rf etc
+    mkdir etc
     cp $1/passwd etc
     cp $1/group etc
 

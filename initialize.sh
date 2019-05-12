9f15de707cf566c9056de779e218f036ac2eac1e
Marcos Mdo <Marcosmdo@knights.ucf.edu>
2019-05-11 20:25:33 -0400

    added testing via pytest

--- initialize.sh
+++ initialize.sh
@@ -11,11 +11,20 @@ then
 
 elif [[ $# -ne 0 ]] # Case for more than one argument passed
 then
-    rm -rf etc
-    mkdir etc
-    cp $1/passwd etc
-    cp $1/group etc
-
-    docker-compose build
-    docker-compose up
+    if [[ $1 == "test" ]]
+    then
+        rm -rf etc
+        mkdir etc
+        cp tests/passwd etc/
+        cp tests/group etc/
+        docker-compose build
+        docker-compose run --rm --entrypoint "python -m pytest --rootdir=/usr/src/app --disable-warnings" password-service
+    else
+        rm -rf etc
+        mkdir etc
+        cp $1/passwd etc
+        cp $1/group etc
+        docker-compose build
+        docker-compose up
+    fi
 fi
---
+++ tests/test_flask.py
@@ -0,0 +1,79 @@
+from password_service.views import configure_routes
+from flask import Flask
+import tempfile
+import pytest
+import urllib
+import json
+import sys
+import os
+
+app = Flask(__name__)
+configure_routes(app)
+
+def test_home_page(client):
+  response = client.get('/')
+  assert response.status_code == 200
+
+def test_get_all_users(client):
+  url = '/users'
+  response = client.get(url)
+  assert response.status_code == 200
+
+def test_get_specific_user(client):
+  total_users = 0
+  with open(os.path.dirname(app.root_path) + '/tests/passwd', 'r') as users:
+    content = users.readline()
+    while content[0] == '#':
+      content = users.readline()
+    while content:
+      user = content.split(':')
+      response = client.get('/users/' + str(user[2]))
+      data = json.loads(response.data)
+      if data['uid'] == user[2]:
+        total_users += 1
+      content = users.readline()
+  assert total_users == 14
+
+def test_get_all_groups(client):
+  url = '/groups'
+  response = client.get(url)
+  assert response.status_code == 200
+
+def test_get_specific_group(client):
+  total_groups = 0
+  with open(os.path.dirname(app.root_path) + '/tests/group', 'r') as groups:
+    content = groups.readline()
+    while content[0] == '#':
+      content = groups.readline()
+    while content:
+      group = content.split(':')
+      response = client.get('/groups/' + str(group[2]))
+      data = json.loads(response.data)
+      if data['gid'] == group[2]:
+        total_groups += 1
+      content = groups.readline()
+  assert total_groups == 42
+
+def test_get_group_query(client):
+  query_string = '?member=username'
+  response = client.get(str('/groups/query' + query_string))
+  data = json.loads(response.data)
+  assert data[1]['name'] == 'dialout'
+
+def test_get_user_query(client):
+  query_string = "?shell=" + urllib.parse.quote('/usr/bin/ksh', safe='')
+  print(query_string, file=sys.stderr)
+  response = client.get('/users/query' + query_string)
+  data = json.loads(response.data)
+  print(data, file=sys.stderr)
+  assert data[2]['name'] == 'paul'
+
+def test_get_groups_from_user(client):
+  response = client.get('/users/4/groups')
+  data = json.loads(response.data)
+  assert data[2]['name'] == 'syslog'
+
+@pytest.fixture
+def client():
+  client = app.test_client()
+  return client
